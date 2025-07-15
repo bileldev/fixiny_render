6ae3228d06f3c6265a9c022bac5742c3abf18b8d
bilel.bennour <bilel@Admin>
2025-07-15 18:33:56 +0100

    adding latest version of fixiny_render

--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -4,10 +4,11 @@
 set -e
 
 FLAG_FILE="/app/.db_seeded"
-
-echo "[init] Waiting for the database to be ready..."
-until nc -z db 5432; do
-  echo "⏳ Waiting for db:5432..."
+DB_HOST=$(echo "$DATABASE_URL" | sed -E 's|.*//.*:.*@(.*):.*\/.*|\1|')
+DB_PORT=$(echo "$DATABASE_URL" | sed -E 's|.*:(5432).*|\1|')
+echo "[init] Waiting for the database to be ready at $DB_HOST:$DB_PORT..."
+until nc -z "$DB_HOST" "$DB_PORT"; do
+  echo "⏳ Waiting for $DB_HOST:$DB_PORT..."
   sleep 1
 done
 
